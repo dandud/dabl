2987a9ed5aa5c6e55f0a8549085a52e9d4bfe524
Daniel Dudley <dan.w.dudley@gmail.com>
2024-08-16 19:18:52 -0400

    updates for dev config and requirements

--- _quickstart_dev.ps1
+++ _quickstart_dev.ps1
@@ -1,4 +1,4 @@
-Invoke-Expression "D:\dev\dabl\dabl\venv\Scripts\activate.ps1"
+Invoke-Expression "D:\dev\dabl\dabl\.venv\Scripts\activate.ps1"
 $env:FLASK_APP = "dabl.py"
 $env:FLASK_DEBUG = "1"
 $env:FLASK_FLASK_CONFIG = "development"
